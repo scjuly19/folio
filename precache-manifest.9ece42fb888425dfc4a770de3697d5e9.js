82b073bc5c67b445c8959359c101afae052e2f26
Sakshi <scjuly69@gmail.com>
2020-08-23 20:47:36 +0530

    Updates

---
+++ precache-manifest.9ece42fb888425dfc4a770de3697d5e9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12e31d9ed91bd797e7458301637a58f",
+    "url": "/folio/index.html"
+  },
+  {
+    "revision": "44c0c65385e9b9d85fff",
+    "url": "/folio/static/css/main.b16060f7.chunk.css"
+  },
+  {
+    "revision": "ec013436a6b8b6a3b5c9",
+    "url": "/folio/static/js/2.ac6658a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/folio/static/js/2.ac6658a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44c0c65385e9b9d85fff",
+    "url": "/folio/static/js/main.edb1a2c4.chunk.js"
+  },
+  {
+    "revision": "ea569fae18c1857cd559",
+    "url": "/folio/static/js/runtime-main.a2fe2300.js"
+  },
+  {
+    "revision": "bc21ae4adcb9bbc12326081fb1f90052",
+    "url": "/folio/static/media/about.bc21ae4a.jpg"
+  },
+  {
+    "revision": "b37b05a713d8eebcbb475119e587859b",
+    "url": "/folio/static/media/github.b37b05a7.png"
+  },
+  {
+    "revision": "f9bdc92be10ead0fc00a0f4dacdcf9fe",
+    "url": "/folio/static/media/instagram.f9bdc92b.png"
+  },
+  {
+    "revision": "86f0d06cfd9f20fd4d00c3405814f70d",
+    "url": "/folio/static/media/me.86f0d06c.jpg"
+  }
+]);
